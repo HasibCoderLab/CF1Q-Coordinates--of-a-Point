241b5e151fc254af6d751131e9dd55a67e86d5c3
Hasib Hasan <coderhasibh@gmail.com>
2026-02-19 15:11:58 +0600

    include basic info

---
+++ Coordinates.cpp
@@ -0,0 +1,8 @@
+/*============================================================
+    Codeforces - CF1-Q.
+    Problem : Data Types & Conditions
+    Contest :1 Data Types & Conditions
+    Topic   : Coordinates of a Point
+    File    : Coordinates.cpp
+    SolvedBy  : Hasib Hasan
+============================================================*/
